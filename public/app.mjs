1a76499c83f77304e80e16d5f5437988a6e811b4
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2022-10-05 15:48:31 -0300

    chore: add javascript frontEnd file

---
+++ public/app.mjs
@@ -0,0 +1,5 @@
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+
+const searchCard = document.querySelectorAll('.search-card');
+
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const routes = require("./src/routes/routes.js");
 const cors = require("cors");
 
+app.use(express.static("public"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-app.use(express.static("public"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}!`);
