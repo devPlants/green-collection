6cffabc04f348b93bce4cf8dcb17b8e9c0ece161
Luana Borges <luanabls@gmail.com>
2022-10-06 19:38:46 -0300

    chore:resolving conflicts in signup page

--- public/modules/signup.mjs
+++ public/modules/signup.mjs
@@ -1,40 +1,40 @@
 import { renderHomeBySignup } from "./app.mjs";
 
 
-export async function signup(){
-    const emailUser= document.querySelector("#email-input");
-    const passwordUser= document.querySelector("#password-input");
+export async function signup() {
+    const emailUser = document.querySelector("#email-input");
+    const passwordUser = document.querySelector("#password-input");
 
     const forms = document.querySelector('#form-signup')
-    
-        const form = new FormData(forms);
 
-    async function getToken(_email, _password){
-    const options = {
-    method: 'POST'
-    };
+    const form = new FormData(forms);
 
-    options.body = form;
+    async function getToken(_email, _password) {
+        const options = {
+            method: 'POST'
+        };
 
-    const response = await fetch('http://localhost:8000/users', options)
-    if (response.status != 201){
-        return alert("Erro de cadastro!")
+        options.body = form;
+
+        const response = await fetch('http://localhost:8000/users', options)
+        if (response.status != 201) {
+            return alert("Erro de cadastro!")
+        }
+        const data = await response.json()
+        const token = await getToken(emailUser.value, passwordUser.value);
+        renderHomeBySignup(token.token, token.userId);
+        return true;
     }
-    const data = await response.json()
-    const token = await getToken(emailUser.value, passwordUser.value);
-    renderHomeBySignup(token.token, token.userId);
-    return true;
-}
 
-    async function getToken(_email, _password){
+    async function getToken(_email, _password) {
         const options = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: `{"email":"${_email}","password":"${_password}"}`
-            };
+        };
         try {
             const response = await fetch('/login', options);
-            if(response.status >= 300){
+            if (response.status >= 300) {
                 return 400;
             }
             const data = await response.json();
@@ -44,3 +44,4 @@ export async function signup(){
         }
 
     }
+}
