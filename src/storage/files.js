9596cd4d288f20b583b69c45a938d2fbee8d737d
Alan <alan.douglas.ads@gmail.com>
2022-10-07 09:40:13 -0300

    chore: add auth

--- src/storage/files.js
+++ src/storage/files.js
@@ -1,8 +1,12 @@
 module.exports = files = {
     getFiles: (req, res) => {
         const fileName = req.params.file;
-        res.status(200).sendFile(
-            __dirname.replace("src/storage", "uploads/") + fileName
-        );
+        try {
+            res.status(200).sendFile(
+                __dirname.replace("src/storage", "uploads/") + fileName
+            );
+        } catch (err) {
+            return res.status(404).end();
+        }
     },
 };
