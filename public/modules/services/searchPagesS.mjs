c19f682baa6d5f26e3fb4f9bc7474100cfbe74b4
Felipe Ozias <oziasfelipe38@gmail.com>
2022-10-09 21:26:08 -0300

    chore: renderizations page search

---
+++ public/modules/services/searchPagesS.mjs
@@ -0,0 +1,21 @@
+import { searchPlantsPageHTML, searchSeedsPageHTML, searchUsersPageHTML, searchLocationPageHTML } from "../pages/searchPagesP.mjs";
+
+export function searchPlantsPageS() {
+
+    return searchPlantsPageHTML();
+}
+
+export function searchSeedsPageS() {
+
+    return searchSeedsPageHTML();
+}
+
+export function searchUsersPageS() {
+
+    return searchUsersPageHTML();
+}
+
+export function searchLocationPageS() {
+
+    return searchLocationPageHTML();
+}
